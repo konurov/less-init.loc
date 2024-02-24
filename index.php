0a2176852bf25cd5932e505f1dcb8339e3cba446
konurov <s_konurov@mail.ru>
2024-02-24 12:15:20 +0600

    My 24.03.2024

--- index.php
+++ index.php
@@ -1,5 +1,61 @@
 <?php 
+error_reporting(-1);
+//
+/* */
+#
+/*
+$_myName // допустимо
+$myName // допустимо
+$__ myvar // допустимо
+$myVar21 // допустимо
+$_1Big // недопустимо - после подчеркивания должна стоять буква
+$1Big // недопустимо - должно начинаться с буквы или символа подчеркивания
+$_er-t // допустимо - содержит не буквенно-цифровой символ (-)
 
+*/
+$Ac = "Hello";
+$title = 'hello world!';
+$title2 = 'page title';
+$fruit = 'apple';
+$winnie_pooh = "Hello, I'm Winnie. I have 2 ".$fruit."s"."<br>";
+//$winnie_pooh = "Hello, I'm Winnie. I have 2 {$fruit}s";
+// $winnieThePooh = "Hello, I'm Winnie";
+echo $winnie_pooh;
+/*
+--- Типы данных в PHP
+    string (строки)
+    integer (целые числа)
+    double (дробные числа)
+    boolean (логический тип)
+    NULL
+    array (массивы)
+    object (объекты)
+    resource (ресурсы) 
+
+*/
+$var = false;
+var_dump($var);
+print($var."<br>");
+print(gettype($var));
+
+
+define("PAge", "new page");
+// define("PAGE", "new page2");
+
+$var = 10;
+$str = 'This is \'string\'. $var';
+
+$str2 = "This is \"string\". $var";
+
+echo $str2;
+
+// HEREDOC
+//NOWDOC
+$sts5 = <<<HER
+This is "string". $var
+HER;
+
+echo $sts5;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +66,13 @@
 </head>
 <body>
    <?php 
-   echo true;
+   echo "<h1>$title2</h1>";
    ?> 
+   <h1><?php echo PAge; ?></h1>
+   
 </body>
 </html>
+Строки | Strings PHP
+https://wm-school.ru/php/php_string.php
+Различные типы чисел в PHP
+https://wm-school.ru/php/php_numbers.php
